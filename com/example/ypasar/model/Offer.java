ae46081cda7fc2ab11b5a804162317132605dccd
yuetiantan <98574258+yuetiantan@users.noreply.github.com>
2022-01-28 15:09:26 +0800

    Add files via upload

---
+++ com/example/ypasar/model/Offer.java
@@ -0,0 +1,21 @@
+package com.example.ypasar.model;
+
+
+public class Offer {
+    String image;
+
+    public Offer(String image) {
+        this.image = image;
+    }
+
+    public Offer() {
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
---
+++ com/example/ypasar/util/SlideAnimationUtil.java
@@ -0,0 +1,38 @@
+package com.example.ypasar.util;
+
+import android.content.Context;
+import android.view.View;
+import android.view.animation.AnimationUtils;
+
+import com.example.ypasar.R;
+
+
+public class SlideAnimationUtil {
+
+
+    public static void slideInFromLeft(Context context, View view) {
+        runSimpleAnimation(context, view, R.anim.slide_from_left);
+    }
+
+    public static void slideOutToLeft(Context context, View view) {
+        runSimpleAnimation(context, view, R.anim.slide_to_left);
+    }
+
+
+    public static void slideInFromRight(Context context, View view) {
+        runSimpleAnimation(context, view, R.anim.slide_from_right);
+    }
+
+
+    public static void slideOutToRight(Context context, View view) {
+        runSimpleAnimation(context, view, R.anim.slide_to_right);
+    }
+
+
+    private static void runSimpleAnimation(Context context, View view, int animationId) {
+        view.startAnimation(AnimationUtils.loadAnimation(
+                context, animationId
+        ));
+    }
+
+}
